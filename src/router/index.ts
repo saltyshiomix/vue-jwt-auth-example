46d80c2350329adc19d6fbb52c6f9e8c9c5cd47c
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2020-01-11 11:36:12 +0900

    Refactoring

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '../views/Home.vue';
-import Login from '../views/Login.vue';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
 
 Vue.use(Router);
 
